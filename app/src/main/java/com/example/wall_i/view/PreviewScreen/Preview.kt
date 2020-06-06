5381614d1de7355b459ce56617ef8ce20f56a9fb
Abhishek38 <abhishekv38@gmail.com>
2020-06-07 00:22:21 +0530

    Preview Screen commit 1

---
+++ app/src/main/java/com/example/wall_i/view/PreviewScreen/Preview.kt
@@ -0,0 +1,13 @@
+package com.example.wall_i.view.PreviewScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wall_i.R
+
+class Preview : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
