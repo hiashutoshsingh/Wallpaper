65d12d028a7499b9ef520c81e4b531372311361c
Ashutosh Kumar Singh <ashutosh.singh@innovaccer.com>
2020-06-06 17:24:23 +0530

    added dependcies

---
+++ app/src/main/java/com/example/wall_i/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.wall_i.utils
+
+object Constants {
+    private const val baseUrl = "https://pixabay.com/api"
+    private const val apiKey = "11809334-b8b2b529a4dd728dc8663833a"
+}
